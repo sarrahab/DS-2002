183716d5b713a0bee4d2f63bef0fec3d26f9db90
sarrahab <101526730+sarrahab@users.noreply.github.com>
2023-03-26 03:18:31 +0000

    Add files via upload

---
+++ sakila.staff.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM sakila.staff
+LIMIT 0, 1000
+
+-- Date: 2023-03-26 03:11
+*/
+INSERT INTO `` (`staff_id`,`first_name`,`last_name`,`address_id`,`picture`,`email`,`store_id`,`active`,`username`,`password`,`last_update`) VALUES (1,'Mike','Hillyer',3,?,'Mike.Hillyer@sakilastaff.com',1,1,'Mike','8cb2237d0679ca88db6464eac60da96345513964','2006-02-15 03:57:16');
+INSERT INTO `` (`staff_id`,`first_name`,`last_name`,`address_id`,`picture`,`email`,`store_id`,`active`,`username`,`password`,`last_update`) VALUES (2,'Jon','Stephens',4,NULL,'Jon.Stephens@sakilastaff.com',2,1,'Jon',NULL,'2006-02-15 03:57:16');
